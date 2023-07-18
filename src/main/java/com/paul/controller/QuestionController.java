1c79421d3309059513daa5f75bead83c9cd1355e
Ankush <ankushpaulclg2002@gmail.com>
2023-07-18 23:22:37 +0530

    second commit

--- src/main/java/com/paul/controller/QuestionController.java
+++ src/main/java/com/paul/controller/QuestionController.java
@@ -1,10 +1,13 @@
 package com.paul.controller;
 
+import com.paul.model.Question;
 import com.paul.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Question")
@@ -13,7 +16,7 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public String getAllQuestions() {
+    public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 }
--- src/main/java/com/paul/service/QuestionService.java
+++ src/main/java/com/paul/service/QuestionService.java
@@ -1,10 +1,19 @@
 package com.paul.service;
 
+import com.paul.dao.QuestionDao;
+import com.paul.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
-    public String getAllQuestions() {
-        return "Questions";
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
     }
 }
